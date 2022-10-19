171cddaccec2e5bceb670d9dc38f2499ef696a70
Caner Borazan <caner.borazan@lunatr.com>
2022-10-19 16:44:24 +0300

    Add project files.

---
+++ KarakterTerstenYazma/Program.cs
@@ -0,0 +1,23 @@
+﻿string str = Console.ReadLine();
+string[] arr = str.Split(" ");
+for (int i = 0; i < arr.Length; i++)
+{
+    string s = arr[i].ToString();
+    string news = "";
+    string h = "";
+    for (int j = 0; j <= s.Length; j++)
+    {
+        if (j == 0)
+            h = s[j].ToString();
+        else if (j == s.Length)
+            news += h;
+        else if (s[j].ToString() == " ")
+        {
+            news += h + " ";
+            h = s[j + 1].ToString();
+        }
+        else
+            news += s[j].ToString();
+    }
+    Console.Write(news + " ");
+}
